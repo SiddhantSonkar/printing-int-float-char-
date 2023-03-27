1217ce5ec17faf77e60808ed04a71f1a1a5656bf
SiddhantSonkar <124968287+SiddhantSonkar@users.noreply.github.com>
2023-03-28 01:15:18 +0530

    Add files via upload

---
+++ class1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=4;                //int - %d
+	float b=9.6;            //float - %f
+	char c='G';             //char - %c
+	
+
+	printf("The value of int= %d \nThe value of float= %f \nThe value of char= %c" ,a,b,c);
+	return 0;
+}
